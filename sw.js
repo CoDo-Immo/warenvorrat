182b35324bd341716faccfcf20de8372ea6fc27c
CoDo-Immo <gian.coray@hotmail.com>
2026-08-11 15:10:31 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker – macht die App installierbar und offline-startfähig.
-const CACHE = "warenvorrat-v4";
+const CACHE = "warenvorrat-v5";
 const ASSETS = [
   "./", "./index.html", "./manifest.json",
   "./icon.svg", "./icon-192.png", "./icon-512.png",
